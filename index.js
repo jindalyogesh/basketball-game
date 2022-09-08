4d265a91c529955a725d6e540a07d1b14c030131
jindalyogesh <jindalyogesh@gmail.com>
2022-09-08 17:24:15 +0530

    scorecard
    the original files

---
+++ index.js
@@ -0,0 +1,37 @@
+// javascript
+let homeScore=0
+let guestScore=0
+ //document.getElementById("home-score").innerText = homeScore
+ //document.getElementById("guest-score").innerText = guestScore
+
+function homeplus1(){
+   
+   homeScore = homeScore +1
+   document.getElementById("home-score").innerText = homeScore
+}
+function homeplus2(){
+   
+    homeScore = homeScore +2
+   document.getElementById("home-score").innerText = homeScore
+}
+function homeplus3(){
+   
+    homeScore = homeScore +3
+   document.getElementById("home-score").innerText = homeScore
+}
+function guestplus1(){
+   
+    guestScore = guestScore +1
+   document.getElementById("guest-score").innerText = guestScore
+}
+
+function guestplus2(){
+   
+    guestScore = guestScore +2
+   document.getElementById("guest-score").innerText = guestScore
+}
+function guestplus3(){
+   
+    guestScore = guestScore +3
+   document.getElementById("guest-score").innerText = guestScore
+}
